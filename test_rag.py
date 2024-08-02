48339160284a452f3df46606e09e7da25af3ff57
Angel Murillo <31391825+ahmurill0217@users.noreply.github.com>
2024-08-01 19:30:00 -0700

    first commit

---
+++ test_rag.py
@@ -0,0 +1,49 @@
+from query_data import query_rag
+from langchain_community.llms.ollama import Ollama
+
+EVAL_PROMPT = """
+Expected Response: {expected_response}
+Actual Response: {actual_response}
+---
+(Answer with 'true' or 'false') Does the actual response match the expected response? 
+"""
+
+
+def test_vial_release():
+    assert query_and_validate(
+        question="What is the GNR number?",
+        expected_response="""61.5""",
+    )
+
+
+def test_vial_release_2():
+    assert query_and_validate(
+        question="What is the cell bank ID?",
+        expected_response="The cell bank ID is 1218-W",
+    )
+
+
+def query_and_validate(question: str, expected_response: str):
+    response_text = query_rag(question)
+    prompt = EVAL_PROMPT.format(
+        expected_response=expected_response, actual_response=response_text
+    )
+    
+    model = Ollama(model="llama3.1")
+    evaluation_results_str = model.invoke(prompt)
+    evaluation_results_str_cleaned = evaluation_results_str.strip().lower()
+
+    print(prompt)
+
+    if "true" in evaluation_results_str_cleaned:
+        # Print response in Green if it is correct.
+        print("\033[92m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return True
+    elif "false" in evaluation_results_str_cleaned:
+        # Print response in Red if it is incorrect.
+        print("\033[91m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return False
+    else:
+        raise ValueError(
+            f"Invalid evaluation result. Cannot determine if 'true' or 'false'."
+        )
